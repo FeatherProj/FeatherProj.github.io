7c39d5fadbd2d360749af28b9251c8d88ceea4cf
Ah_Roon <ahroon2979@gmail.com>
2021-02-23 14:35:50 +0900

    Major Website Overhaul

---
+++ err01.js
@@ -0,0 +1,3 @@
+function pageNotAvailable() {
+    alert("Sorry, this page is still under construction. Come back later!");
+}
---
+++ materialize.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(function() {
+        // Plugin initialization
+        $('select').not('.disabled').formSelect();
+    });
+})(jQuery); // end of jQuery name space
+
+(function($) {
+    $(function() {
+        $('.sidenav').sidenav();
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
